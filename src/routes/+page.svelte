55edbaf8638d0e8a1112ef0f19529ba9bd6961d3
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-10-04 12:13:21 -0700

    Install Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="/page1">Page1</a>
+
+<h1 class="text-3xl font-bold">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
