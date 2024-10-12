596914535ec6ea918c79464e386326cf5cdd6121
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-10-11 22:15:22 -0700

    Add design system colors to Tailwind config, and as CSS variables in app.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary00: '#000000',
+				primary08: '#1a170f',
+				primary18: '#3a3322',
+				primary28: '#5b4f34',
+				primary38: '#7b6b47',
+				primary58: '#b19f77',
+				primary68: '#c3b697',
+				primary78: '#d6cdb8',
+				primary88: '#e9e4d8',
+				primary94: '#f4f2ec',
+				primary97: '#f9f8f5',
+				primary100: '#ffffff'
+			}
+		}
 	},
 	plugins: []
 };
