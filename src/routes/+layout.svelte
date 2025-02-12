2571d324fc157c2f5ab45ed4ff5278b2ed9cac5d
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-02-11 19:37:15 -0800

    refactor(footer): Add arrow-up icon svg to Icon component. Replace text with arrow-up icon on layout footer. Add spacer div in footer to match width of arrow-up.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import Header from '../components/Header.svelte';
 	import type { Snippet } from 'svelte';
+	import Icon from '../components/Icon.svelte';
 
 	interface Props {
 		children?: Snippet;
@@ -15,13 +16,14 @@
 <Header {y} />
 {@render children?.()}
 <div class="flex justify-between px-6">
-	<br />
+	<div class="w-[24px]"></div>
 	<p id="copyright" class="text-xs text-center text-primary48 py-10">
 		Copyright © {currentYear} LydianLab Music Education Technology
 	</p>
 	<button
 		class="text-xs text-primary48"
-		onclick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Back to Top</button
+		onclick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+		><Icon name="arrow-up" className="fill-current" size="24" /></button
 	>
 </div>
 
